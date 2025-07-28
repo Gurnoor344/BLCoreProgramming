4f7171104e8571153976b4ebd72b4b7f8ea13a33
gurnoor <gurnoor344.be22@chitkara.edu.in>
2025-07-28 22:32:18 +0530

    Commit

---
+++ PrintArrays.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class PrintArrays{
+	public static void main(String[] args){
+		int[] age ={24,23,18,19,20};
+		System.out.println("first element is " + age[0]);
+		System.out.println("first element is " + age[1]);
+		System.out.println("first element is " + age[2]);
+		System.out.println("first element is " + age[3]);
+	}
+}
+		
+		
+		
