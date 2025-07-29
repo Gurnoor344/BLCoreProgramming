f0aa85b980a0b8c618cdce652c52dce354aa5142
gurnoor <gurnoor344.be22@chitkara.edu.in>
2025-07-29 22:19:22 +0530

    MaxDigit

---
+++ Level2/MaxDigit.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class MaxDigit {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] digits = new int[10];  // Array to store digits
+        int index = 0;
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        
+        while (num != 0 && index < 10) {
+            digits[index] = num % 10;  
+            num = num / 10;           
+            index++;                   
+        }
+        int largest = 0;
+        int secondLargest = 0;
+		
+        for (int i = 0; i < index; i++) {
+            if (digits[i] > largest) {
+                secondLargest = largest;
+                largest = digits[i];
+            } else if (digits[i] > secondLargest && digits[i] != largest) {
+                secondLargest = digits[i];
+            }
+        }
+        System.out.println("Largest digit: " + largest);
+        System.out.println("Second largest digit: " + secondLargest);
+    }
+}
