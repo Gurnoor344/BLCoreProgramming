1ba57a0d02b951d5dfc0f5e17eaff39fee88893b
gurnoor <gurnoor344.be22@chitkara.edu.in>
2025-07-30 09:36:28 +0530

    subject grade question

---
+++ Level2/SubjectQuestion.java
@@ -0,0 +1,74 @@
+import java.util.Scanner;
+
+public class SubjectQuestion {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+       
+        System.out.print("Enter number of students: ");
+        int n = sc.nextInt();
+
+       
+        int[] physics = new int[n];
+        int[] chemistry = new int[n];
+        int[] maths = new int[n];
+        double[] percentage = new double[n];
+        char[] grade = new char[n];
+
+       
+        for (int i = 0; i < n; i++) {
+            System.out.println("\nEnter marks for Student " + (i + 1) + ":");
+
+            
+            System.out.print("Physics: ");
+            physics[i] = sc.nextInt();
+            if (physics[i] < 0) {
+                System.out.println("Marks cannot be negative. Enter again.");
+                i--;
+                continue;
+            }
+
+            
+            System.out.print("Chemistry: ");
+            chemistry[i] = sc.nextInt();
+            if (chemistry[i] < 0) {
+                System.out.println("Marks cannot be negative. Enter again.");
+                i--;
+                continue;
+            }
+
+            
+            System.out.print("Maths: ");
+            maths[i] = sc.nextInt();
+            if (maths[i] < 0) {
+                System.out.println("Marks cannot be negative. Enter again.");
+                i--;
+                continue;
+            }
+
+            
+            int total = physics[i] + chemistry[i] + maths[i];
+            percentage[i] = total / 3.0;
+
+           
+            if (percentage[i] >= 90) grade[i] = 'A';
+            else if (percentage[i] >= 80) grade[i] = 'B';
+            else if (percentage[i] >= 70) grade[i] = 'C';
+            else if (percentage[i] >= 60) grade[i] = 'D';
+            else if (percentage[i] >= 50) grade[i] = 'E';
+            else grade[i] = 'F';
+        }
+
+        System.out.println("\n--- Student Report ---");
+        for (int i = 0; i < n; i++) {
+            System.out.println("\nStudent " + (i + 1));
+            System.out.println("Physics: " + physics[i]);
+            System.out.println("Chemistry: " + chemistry[i]);
+            System.out.println("Maths: " + maths[i]);
+            System.out.println("Percentage: " + percentage[i] + "%");
+            System.out.println("Grade: " + grade[i]);
+        }
+
+        sc.close();
+    }
+}
